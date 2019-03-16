be2eeeabbddc7aa212c2b778ff42848aad1ebb95
Lenilson Tomaz <Lenilsonjnr@gmail.com>
2019-03-15 22:26:39 -0300

    Atualização na main e na classe exception

--- PilhaComListaLigadaSimples/pilhall/PilhaVaziaException.java
+++ PilhaComListaLigadaSimples/pilhall/PilhaVaziaException.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pilhall;
 
-/**
- *
- * @author priscila06
- */
 public class PilhaVaziaException extends Exception   {
     
     public PilhaVaziaException(String msg){
@@ -16,5 +7,4 @@ public class PilhaVaziaException extends Exception   {
     }
 
  
-    
 }
